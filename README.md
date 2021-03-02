658ef23ddae0c06782baa4ac593b57ace4307e1e
Maksim Pecherskiy <max@maksimize.com>
2021-03-02 13:26:20 -0500

    Updating with generated hugo css.

--- README.md
+++
@@ -1,7 +0,0 @@
-# WB Data Lab Intranet Page
-
-## About
-This single-page website provides information about the SD Data Lab Community of Practice activities. 
-
-## Source Code Copyright and License
-I used Start Bootstrap, an open source library of free Bootstrap templates and themes, created and maintained by **[David Miller](http://davidmiller.io/)**, Owner of [Blackrock Digital](http://blackrockdigital.io/). Copyright 2013-2019 Blackrock Digital LLC. Code released under the [MIT](https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/gh-pages/LICENSE) license.
---
+++ js/functions/workshops-and-clinics-table.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const mostRecent = document.querySelector(".most-recent")
+  const tableDates = document.querySelectorAll(".date")
+
+  const formatIntoDateType = function (stringDate) {
+    // Convert string date into Date type.
+    if (typeof stringDate === "string") {
+      return new Date(stringDate).toISOString().split("T")[0] // Formats date into YYYY-MM-DD format.
+    } else {
+      throw new Error(
+        "The argument provided is not a string. Please check your arguments type."
+      )
+    }
+  }
+
+  if (mostRecent) {
+    mostRecent.textContent = formatIntoDateType(mostRecent.textContent)
+  }
+
+  tableDates.forEach((date) => {
+    date.textContent = formatIntoDateType(date.textContent)
+  })
+
+
+  const dataTable = new simpleDatatables.DataTable(
+    "#workshops-and-clinics-table",
+    {
+      searchable: true,
+      fixedHeight: true,
+      perPage: 25,
+    }
+  )
+  const removeTableHeightFromContainer = function (table) {
+    table.container.style.height = 'unset'
+  }
+
+  removeTableHeightFromContainer(dataTable)
+})
